c60cd396b20dbc44b5d9f2db30045bdaa28261ef
Stefan B <s.bartos@outlook.com>
2013-03-11 21:14:03 +0100

    trying to load an image; implemented car as a sprite

--- 2.0/car.py
+++ 2.0/car.py
@@ -1,20 +1,41 @@
-class Car:
+import os, sys
+import pygame
+from pygame.locals import *
+
+class Car(pygame.sprite.Sprite):
 	#lock...Lenkeinschlag
-	def __init__ (mass,power,currentSpeed,maxSpeed,x,y,currentLock, maxLock, orientation):
-		self.mass = mass
-		self.power = power
-		self.currSpeed = currentSpeed
-		self.maxSpeed = maxSpeed
-		self.x = x
-		self.y = y
-		self.currLock = currentLock
-		self.maxLock = maxLock
-		self.orientation = orientation
-	
-	def updatePosition(self,newX,newY,newAngle):
-		self.x = newX
-		self.y = newY
-		self.orientation = newAngle
-	
-	def nextPos(time):
-		
+    def __init__ (self,mass,power,currentSpeed,maxSpeed,x,y,currentLock, maxLock, orientation):
+        pygame.sprite.Sprite.__init__(self)
+        self.mass = mass
+        self.power = power
+        self.currSpeed = currentSpeed
+        self.maxSpeed = maxSpeed
+        self.x = x
+        self.y = y
+        self.currLock = currentLock
+        self.maxLock = maxLock
+        self.orientation = orientation
+        self.image, self.rect = self.load_image('car.png')
+        self.punching = 0
+
+    def updatePosition(self,newX,newY,newAngle):
+        self.x = newX
+        self.y = newY
+        self.orientation = newAngle
+
+    def nextPos(time):
+        pass
+
+    def load_image(name, colorkey=None):
+        fullname = os.path.join('images', name)
+        try:
+            image = pygame.image.load(fullname)
+        except pygame.error, message:
+            print 'Cannot load image:', fullname
+            raise SystemExit, message
+        image = image.convert()
+        """ if colorkey is not None:
+            if colorkey is -1:
+                colorkey = image.get_at((0,0))
+            image.set_colorkey(colorkey, RLEACCEL)"""
+        return image, image.get_rect()
--- 2.0/gui.py
+++ 2.0/gui.py
@@ -1,36 +1,48 @@
 import os, sys
 import pygame
+from car import Car
 from pygame.locals import *
 
 class Gui:
 	
-	def __init__ (self, width, heigth):
-		self.width = width
-		self.heigth = heigth
-		self.windows = pygame.display.set_mode((width,heigth))
-		pygame.display.set_caption("Neuro Junk 2012/13")
-		self.screen = pygame.display.get_surface()
-		self.drawStartUp()
-	
-	def drawStartUp(self):
-		colors = {'black':(0,0,0),'white':(255,255,255),'pink':(255,0,255)}
-		self.screen.fill(colors['black'])
-		self.run()
-		
-	def input(self,events):
-		for event in events:
-			if event.type == QUIT:
-				sys.exit(0)
-			elif event.type == KEYDOWN and event.key == 113: #Q
-				sys.exit(0)
-			elif event.type == KEYDOWN and event.key == 273: #Arrow_UP
-				pass
-			else:
-				print event
-	
-	def run(self):
-		while True:
-			self.input(pygame.event.get())
+    def __init__ (self, width, heigth):
+        self.width = width
+        self.heigth = heigth
+        self.windows = pygame.display.set_mode((width,heigth))
+        pygame.display.set_caption("Neuro Junk 2012/13")
+        self.screen = pygame.display.get_surface()
+        self.colors = colors = {'black':(0,0,0),'white':(255,255,255),'pink':(255,0,255)}
+        self.car = Car(1400,66,0,170,80,80,0,50,0)
+        self.drawBackground()
+        self.drawCar()
+
+    def drawBackground(self):
+        self.screen.fill(colors['black'])
+        self.update()
+        self.run()
+    
+    def drawCar(self):
+        allsprites = pygame.sprite.RenderPlain(self.car)
+        allsprites.draw(self.screen)
+        update()
+        
+    def input(self,events):
+        for event in events:
+            if event.type == QUIT:
+                sys.exit(0)
+            elif event.type == KEYDOWN and event.key == 113: #Q
+                sys.exit(0)
+            elif event.type == KEYDOWN and event.key == 273: #Arrow_UP
+                pass
+            else:
+                print event
+                
+    def update(self):
+        pygame.display.update()
+
+    def run(self):
+        while True:
+            self.input(pygame.event.get())
 		
 if  __name__ =='__main__':
 	Gui(800,400)

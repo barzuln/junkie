8898fbbbe2ce386c37ee3db1d49e6edcc01c188d
j33t <jauernig.michi@gmail.com>
2013-03-13 08:54:51 +0100

    nextPos Methode

--- 2.0/car.py
+++ 2.0/car.py
@@ -24,7 +24,46 @@ class Car(pygame.sprite.Sprite):
         self.orientation = newAngle
 
     def nextPos(time):
-        pass
+        	
+		phi = Car.currLock
+		
+		v2 = Car.currSpeed + getAcc() * time
+		va = ( v2 + Car.currSpeed) / 2
+		s = va * time
+		r = ((s/2)/(math.sin(math.radians( phi/2))))*(math.sin(math.radians((180-phi)/2)))
+		eps= phi/2
+		
+		x=math.sin(math.radians(eps)) * ((s/2)/(math.sin(math.radians(180 - phi))))
+		
+		if x == 0:
+			b = s * math.sin(math.radians(Car.orientation + eps))
+			a = s * math.sin(math.radians(90 - (Car.orientation + eps)))
+		else:
+			b = 2 * x * math.sin(math.radians(Car.orientation + eps))
+			a = 2 * x * math.sin(math.radians(90 - (Car.orientation + eps)))
+		
+		
+		q = Car.orientation + eps 
+		if 0 < q < 90:
+			#1.Quadrant
+			x2 = car.x + a
+			y2 = car.y + b
+			car.updatePosition(x2, y2, (Car.orientation + eps))
+		elif 90 < q < 180:
+			#2.Quadrant
+			x2 =  car.x - a
+			y2 = car.y + b
+			car.updatePosition(x2, y2, (Car.orientation + eps))
+		elif 180 < q < 270:
+			#3.Quadrant
+			x2 =  car.x - a
+			y2 = car.y - b
+			car.updatePosition(x2, y2, (Car.orientation + eps))
+		else: 
+			#4.Quadrant
+			x2 =  car.x + a
+			y2 = car.y - b
+			car.updatePosition(x2, y2, (Car.orientation + eps))
 
     def load_image(name, colorkey=None):
         fullname = os.path.join('images', name)

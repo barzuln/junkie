90ab9165dc03509d4eec602fd4abc0c0308b425b
Barzuln <bar13540@spengergasse.at>
2013-03-13 09:03:33 +0100

    <= ifs

--- 2.0/car.py
+++ 2.0/car.py
@@ -48,17 +48,17 @@ class Car(pygame.sprite.Sprite):
 		
 		
 		q = Car.orientation + eps 
-		if 0 < q < 90:
+		if 0 < q <= 90:
 			#1.Quadrant
 			x2 = car.x + a
 			y2 = car.y + b
 			car.updatePosition(x2, y2, (Car.orientation + eps))
-		elif 90 < q < 180:
+		elif 90 < q <= 180:
 			#2.Quadrant
 			x2 =  car.x - a
 			y2 = car.y + b
 			car.updatePosition(x2, y2, (Car.orientation + eps))
-		elif 180 < q < 270:
+		elif 180 < q <= 270:
 			#3.Quadrant
 			x2 =  car.x - a
 			y2 = car.y - b
@@ -68,17 +68,3 @@ class Car(pygame.sprite.Sprite):
 			x2 =  car.x + a
 			y2 = car.y - b
 			car.updatePosition(x2, y2, (Car.orientation + eps))
-
-    def load_image(name, colorkey=None):
-        fullname = os.path.join('images', name)
-        try:
-            image = pygame.image.load(fullname)
-        except pygame.error, message:
-            print 'Cannot load image:', fullname
-            raise SystemExit, message
-        image = image.convert()
-        """ if colorkey is not None:
-            if colorkey is -1:
-                colorkey = image.get_at((0,0))
-            image.set_colorkey(colorkey, RLEACCEL)"""
-        return image, image.get_rect()

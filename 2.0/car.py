7fd789a970114c1f5b3bbb429ebc8dc1c1e568d5
Barzuln <bar13540@spengergasse.at>
2013-03-18 12:38:57 +0100

    fixed michis error

--- 2.0/car.py
+++ 2.0/car.py
@@ -5,89 +5,104 @@ import math
 
 class Car:
 	#lock...Lenkeinschlag
-    def __init__ (self,mass,acceleration,currentSpeed,maxSpeed,pos,currentLock, maxLock, orientation, size, innerPoly, outerPoly):
-        self.mass = mass #kg
-        self.acceleration = acceleration #meters/seconds
-        self.currSpeed = currentSpeed #meters/seconds
-        self.maxSpeed = maxSpeed #meters/seconds
-        self.position = pos #meter
-        self.currLock = currentLock
-        self.maxLock = maxLock
-        self.orientation = orientation
-        self.size = size #in meter 
-        self.innerPoly = innerPoly #course Points inside
-        self.outerPoly = outerPoly #course Points outside        
-    
-    def updatePosition(self,newX,newY,newAngle):
-        self.position = (newX,newY)
-        self.orientation = newAngle
-        
-    def convertSizeToPixel(self): #from meters to pixel
-        return (self.size[0]*3.5,self.size[1]*3.5)
-    
-    def convertPositionToPixel(self): #from meters to pixel
-        return (self.position[0]*3.5,self.position[1]*3.5)
-        
-    def convertPolygonToMeters(self, pixel):
-        return (pixel[0]/3.5,pixel[1]/3.5)
-    
-    def nextPos(self, time):
-        phi = self.currLock
-        v2 = self.currSpeed + self.acceleration * time
-        va = ( v2 + self.currSpeed) / 2
-        s = va * time
-        #r = ((s/2)/(math.sin(math.radians(phi/2))))*(math.sin(math.radians((180-phi)/2)))
-        eps = phi/2
+	def __init__ (self,mass,acceleration,currentSpeed,maxSpeed,pos,currentLock, maxLock, orientation, size, innerPoly, outerPoly):
+		self.mass = mass #kg
+		self.acceleration = acceleration #meters/seconds
+		self.currSpeed = currentSpeed #meters/seconds
+		self.maxSpeed = maxSpeed #meters/seconds
+		self.position = pos #meter
+		self.currLock = currentLock
+		self.maxLock = maxLock
+		self.orientation = orientation
+		self.size = size #in meter 
+		self.innerPoly = innerPoly #course Points inside
+		self.outerPoly = outerPoly #course Points outside        
 
-        x = math.sin(math.radians(eps)) * ((s/2)/(math.sin(math.radians(180 - phi))))
+	def updatePosition(self,newX,newY,newAngle):
+		self.position = (newX,newY)
+		self.orientation = newAngle
+		
+	def convertSizeToPixel(self): #from meters to pixel
+		return (self.size[0]*3.5,self.size[1]*3.5)
 
-        if x == 0:
-            b = s * math.sin(math.radians(self.orientation + eps))
-            a = s * math.sin(math.radians(90 - (self.orientation + eps)))
-        else:
-            b = 2 * x * math.sin(math.radians(self.orientation + eps))
-            a = 2 * x * math.sin(math.radians(90 - (self.orientation + eps)))
+	def convertPositionToPixel(self): #from meters to pixel
+		return (self.position[0]*3.5,self.position[1]*3.5)
+		
+	def convertPolygonToMeters(self, pixel):
+		return (pixel[0]/3.5,800-pixel[1]/3.5)
 
-        q = self.orientation + eps 
-        
-        if 0 < q <= 90: #1.Quadrant
-            x2 = self.position[0] + a
-            y2 = self.position[1] + b
-        elif 90 < q <= 180:
-            #2.Quadrant
-            x2 =  self.position[0] - a
-            y2 = self.position[1] + b
-        elif 180 < q <= 270: #3.Quadrant
-            x2 =  self.position[0] - a
-            y2 = self.position[1] - b
-        else: #4.Quadrant
-            x2 =  self.position[0] + a
-            y2 = self.position[0] - b
-            
-        if self.collision(self.innerPoly, (self.position[0], self.position[0]), (x2, y2)) and self.collision(self.outerPoly, (self.position[0], self.position[0]), (x2, y2)):	
-            self.updatePosition(x2, y2, (self.orientation + eps))	
-        else:
-            print "HURNS WAND!"
+	def nextPos(self, time):
+		phi = self.currLock
+		print "phi " + str(phi)
+		v2 = self.currSpeed + self.acceleration * time
+		va = ( v2 + self.currSpeed) / 2
+		s = va * time
+		#r = ((s/2)/(math.sin(math.radians(phi/2))))*(math.sin(math.radians((180-phi)/2)))
+		eps = phi/2
+		print "eps: " + str(eps)
+		x = (math.sin(math.radians(eps)) * ((s/2)/(math.sin(math.radians((180 - eps))))))
+		print "x: " + str(x)
 
-    def collision(self, polygon, carpos1, carpos2):
-        
-        for i in range(0, len(polygon)-1, 1):
-            p1 = self.convertPolygonToMeters(polygon[i])
-            p2 = 0
-            if i < (len(polygon)-1):
-                p2 = self.convertPolygonToMeters(polygon[i + 1])
-            else:
-                p2 = self.convertPolygonToMeters(polygon[0])
-            
-            k = (p2[1] - p1[1]) / (p2[0] - p1[0])
-            d = p1[1] - k * p1[0]
-            
-            k2 = (carpos2[1] - carpos1[1])/(carpos2[0] - carpos1[0])
-            d2 = carpos1[1] - k * carpos1[0]
-            
-            if k != k2:
-                xs = (d2 - d) / (k - k2)
-                            
-            if min(carpos1[0], carpos2[0]) < xs < max(carpos1[0], carpos2[0]):
-                return True
-        return False 
+		if x == 0:
+			b = s * math.sin(math.radians(self.orientation))
+			a = s * math.sin(math.radians(90 - (self.orientation)))
+		else:
+			b = 2 * x * math.sin(math.radians(self.orientation + eps))
+			a = 2 * x * math.sin(math.radians(90 - (self.orientation + eps)))
+
+		q = self.orientation + eps 
+		print "q: " + str(q)
+		
+		if 0 < q <= 90: #1.Quadrant
+			x2 = self.position[0] + a
+			y2 = self.position[1] + b
+		elif 90 < q <= 180:
+			#2.Quadrant
+			x2 =  self.position[0] - a
+			y2 = self.position[1] + b
+		elif 180 < q <= 270: #3.Quadrant
+			x2 =  self.position[0] - a
+			y2 = self.position[1] - b
+		else: #4.Quadrant
+			x2 =  self.position[0] + a
+			y2 = self.position[1] - b
+			
+		if not self.collision(self.innerPoly, (self.position[0], self.position[1]), (x2, y2)) and not self.collision(self.outerPoly, (self.position[0], self.position[1]), (x2, y2)):	
+			self.updatePosition(x2, y2, (self.orientation + eps))	
+		else:
+			print "WAND!"
+			print self.position
+			print str(x2) + str(y2)
+		self.currSpeed = v2
+			
+	def collision(self, polygon, carpos1, carpos2):
+		for i in range(0, len(polygon)-1, 1):
+			p1 = self.convertPolygonToMeters(polygon[i])
+			p2 = 0.0
+			k = 0.0
+			k2 = 0.0
+			if i < (len(polygon)-1):
+				p2 = self.convertPolygonToMeters(polygon[i + 1])
+			else:
+				p2 = self.convertPolygonToMeters(polygon[0])
+			if p2[0] == p1[0] and p2[1] == p1[1]:
+				return False
+			if p2[0] == p1[0]:
+				k = 0.0
+			else:
+				k = (p2[1] - p1[1]) / (p2[0] - p1[0])
+			d = p1[1] - k * p1[0]
+			if carpos2[0] == carpos1[0] and carpos2[1] - carpos1[1]:
+				return False
+			if carpos2[0] == carpos1[0]:
+				k2 = 0.0
+			else:
+				k2 = (carpos2[1] - carpos1[1])/(carpos2[0] - carpos1[0])
+			d2 = carpos1[1] - k * carpos1[0]
+			
+			if k != k2:
+				xs = (d2 - d) / (k - k2)
+							
+			if min(carpos1[0], carpos2[0]) < xs < max(carpos1[0], carpos2[0]):
+				return True
+		return False 
--- 2.0/gui.py
+++ 2.0/gui.py
@@ -6,48 +6,54 @@ import time
 
 class Gui:
 
-    def __init__ (self, width, heigth):
-        self.width = width
-        self.heigth = heigth
-        self.windows = pygame.display.set_mode((width,heigth))
-        pygame.display.set_caption("Neuro Junk 2012/13")
-        self.outerPoly = [(100,215),(230,100),(400,150),(560,105),(650,180),(685,270),(710,400),(615,500),(510,545),(355,540),(335,480),(255,490),(185,540),(125,521),(90,420)]
-        self.innerPoly = [(150,215),(230,150),(400,200),(540,155),(600,210),(625,275),(650,380),(600,440),(510,500),(380,480),(380,420),(260,430),(185,500),(150,480)]
-        self.car = Car(1400,1.43,0,50,(85,130),0, 38, 180,(4,3),self.innerPoly, self.outerPoly) #self,mass,acceleration,currentSpeed,maxSpeed,pos,currentLock, maxLock, orientation, length, width
-        self.screen = pygame.display.get_surface()
-        self.drawStartUp()
+	def __init__ (self, width, heigth):
+		self.width = width
+		self.heigth = heigth
+		self.windows = pygame.display.set_mode((width,heigth))
+		pygame.display.set_caption("Neuro Junk 2012/13")
+		self.outerPoly = [(100,215),(230,100),(400,150),(560,105),(650,180),(685,270),(710,400),(615,500),(510,545),(355,540),(335,480),(255,490),(185,540),(125,521),(90,420)]
+		self.innerPoly = [(150,215),(230,150),(400,200),(540,155),(600,210),(625,275),(650,380),(600,440),(510,500),(380,480),(380,420),(260,430),(185,500),(150,480)]
+		self.car = Car(1400,1.43,50.0,50.0,(85.0,130.0),1.0, 38.0, 0.0,(4,3),self.innerPoly, self.outerPoly) #self,mass,acceleration,currentSpeed,maxSpeed,pos,currentLock, maxLock, orientation, length, width
+		self.colors = {'black':(0,0,0),'white':(255,255,255),'pink':(255,0,255)}
+		self.screen = pygame.display.get_surface()
+		self.drawStartUp()
 
-    def drawStartUp(self):
-        colors = {'black':(0,0,0),'white':(255,255,255),'pink':(255,0,255)}
-        self.screen.fill(colors['white'])
-        carsize = self.car.convertSizeToPixel()
-        pygame.draw.rect(self.screen, colors['pink'], pygame.Rect(self.car.convertPositionToPixel()[0], self.car.convertPositionToPixel()[1], carsize[0], carsize[1]))#drawing the car
-        pygame.draw.polygon(self.screen, colors['black'], self.outerPoly,2)
-        pygame.draw.polygon(self.screen, colors['black'], self.innerPoly,2)
-        self.update()
-        self.run()
-        
-    def update(self):
-        pygame.display.update()
-        
+	def drawStartUp(self):
+		self.screen.fill(self.colors['white'])
+		self.carsize = self.car.convertSizeToPixel()
+		self.carRect = pygame.draw.rect(self.screen, self.colors['pink'], pygame.Rect(self.car.convertPositionToPixel()[0], self.car.convertPositionToPixel()[1], self.carsize[0], self.carsize[1]))#drawing the car
+		pygame.draw.polygon(self.screen, self.colors['black'], self.outerPoly,2)
+		pygame.draw.polygon(self.screen, self.colors['black'], self.innerPoly,2)
+		self.update()
+		self.run()
+		
+	def update(self):
+		self.screen.fill(self.colors['white'])
+		pygame.draw.polygon(self.screen, self.colors['black'], self.outerPoly,2)
+		pygame.draw.polygon(self.screen, self.colors['black'], self.innerPoly,2)
+		self.carRect = pygame.draw.rect(self.screen, self.colors['pink'], pygame.Rect(self.car.convertPositionToPixel()[0], self.car.convertPositionToPixel()[1], self.carsize[0], self.carsize[1]))
+		pygame.display.update()
+		
 
-    def input(self,events):
-        for event in events:
-            if event.type == QUIT:
-                sys.exit(0)
-            elif event.type == KEYDOWN and event.key == 113: #Q
-                sys.exit(0)
-            elif event.type == KEYDOWN and event.key == 273: #Arrow_UP
-                self.car.currSpeed += 1
-                print self.car.currSpeed
-            else:
-                pass
+	def input(self,events):
+		for event in events:
+			if event.type == QUIT:
+				sys.exit(0)
+			elif event.type == KEYDOWN and event.key == 113: #Q
+				sys.exit(0)
+			elif event.type == KEYDOWN and event.key == 273: #Arrow_UP
+				self.car.currSpeed += 1
+				print self.car.currSpeed
+			else:
+				pass
 
-    def run(self):
-        while True:
-            #self.car.nextPos(1)
-            time.sleep(1)
-            self.input(pygame.event.get())
+	def run(self):
+		while True:
+			self.car.nextPos(0.001)
+			self.update()
+			print self.car.position
+			time.sleep(0.001)
+			self.input(pygame.event.get())
 
 if  __name__ =='__main__':
 	Gui(800,600)
